51e0c845ab1325bb3da22ceb241c90079adb2f5f
Swadhin Saraf <connectswadhin@gmail.com>
2019-05-30 17:49:23 +0530

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,33 @@
+// const MongoClient= require('mongodb').MongoClient;
+const {MongoClient, ObjectID}= require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db)=>{
+if (err) {
+  return console.log('unable to connect to the database');
+};
+console.log('database connected successfully');
+
+
+// db.collection('Todos').insertOne({
+//   text:'Something to do',
+// completed:false}, (err,result) =>{
+//     if(err){
+//       return console.log('unable to add to collection', err);
+//     };
+// console.log(JSON.stringify(result.ops,undefined,2));
+// });
+
+db.collection('Users').insertOne({
+  name:'Swadhin',
+  age:25,
+  location:'Delhi'
+}, (err,result) =>{
+  if (err) {
+    return console.log('unable to add user', err);
+
+  };
+  console.log(JSON.stringify(result.ops,undefined,2));
+  console.log(result.ops[0]._id.getTimestamp());
+});
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,25 @@
+// const MongoClient= require('mongodb').MongoClient;
+const {MongoClient, ObjectID}= require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db)=>{
+if (err) {
+  return console.log('unable to connect to the database');
+};
+console.log('database connected successfully');
+
+ // db.collection('Todos').find().count().then((count) =>{
+ //   console.log(`Todos count:${count} `);},
+ // (err)=> {
+ //  console.log("unable to fetch database", err);
+ //   });
+
+db.collection('Users').find({name:"Swadhin"}).toArray().then((name) =>{
+  console.log('Users');
+  console.log(JSON.stringify(name,undefined,2));},
+  (err) =>{
+    console.log('unable to fetch data', err);
+  }
+);
+    //
+    // db.close();
+});
